22c64f451b6efae55aa68d338438eaf87fff6d37
Alexsandro Bezerra <alex@pop-os.localdomain>
2020-11-03 13:55:24 -0300

    Criando repositórios

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,18 +1,19 @@
-import { parseISO, startOfHour, isEqual } from 'date-fns'
+import { parseISO, startOfHour } from 'date-fns'
 import { Router } from 'express'
 
-import Appointment from '../models/Appointment'
+import AppointmentRepository from '../repositories/AppointmentsRepository'
 
 const appointmentsRouter = Router()
 
-const appointments: Appointment[] = []
+const appointmentsRepository = new AppointmentRepository()
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body
 
   const parsedDate = startOfHour(parseISO(date))
-  const findAppointmentInSameDate = appointments.find(appointment =>
-    isEqual(parsedDate, appointment.date)
+
+  const findAppointmentInSameDate = appointmentsRepository.findByDate(
+    parsedDate
   )
 
   if (findAppointmentInSameDate) {
@@ -21,9 +22,7 @@ appointmentsRouter.post('/', (request, response) => {
       .json({ message: 'This appointment is already booked' })
   }
 
-  const appointment = new Appointment(provider, parsedDate)
-
-  appointments.push(appointment)
+  const appointment = appointmentsRepository.create(provider, parsedDate)
 
   return response.json(appointment)
 })

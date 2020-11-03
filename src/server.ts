077c9d3cceeedd1f0d824fa46b76427df66d0fe8
Alexsandro Bezerra <alex@pop-os.localdomain>
2020-11-03 10:00:18 -0300

    Configurando estrutura

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+
+import routes from './routes'
+
+const app = express()
+const PORT = process.env.PORT || 3333
+
+app.use(routes)
+
+app.listen(PORT, () => {
+  console.log(`🚀️ Server running on port ${PORT}`)
+})

b798f5a04200648dd0549b83c14f1c8377b873c4
Alexsandro G Bezerra <aalexo1313@gmail.com>
2020-11-07 10:06:51 -0300

    Criando o módulo de agendamento

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -1,15 +1,13 @@
-import { v4 as uuid } from 'uuid'
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm'
 
+@Entity('appointments')
 export default class Appointment {
+  @PrimaryGeneratedColumn('uuid')
   id: string
 
+  @Column()
   provider: string
 
+  @Column('time with time zone')
   date: Date
-
-  constructor({ provider, date }: Omit<Appointment, 'id'>) {
-    this.id = uuid()
-    this.provider = provider
-    this.date = date
-  }
 }

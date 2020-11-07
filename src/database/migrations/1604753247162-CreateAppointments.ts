b8e0cab3a41de137dd3a2c2aed33a057cba5008c
Alexsandro G Bezerra <aalexo1313@gmail.com>
2020-11-07 10:29:40 -0300

    Resitório do TypeORM

--- src/database/migrations/1604753247162-CreateAppointments.ts
+++ src/database/migrations/1604753247162-CreateAppointments.ts
@@ -10,7 +10,8 @@ export class CreateAppointments1604753247162 implements MigrationInterface {
             name: 'id',
             type: 'varchar',
             isPrimary: true,
-            generationStrategy: 'uuid'
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()'
           },
           {
             name: 'provider',

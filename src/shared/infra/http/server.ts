33a1ea13c6e3d360fd359a2b94c232e12a8a7a18
Alexsandro G Bezerra <alexsandro.bezerra@outlook.com.br>
2020-11-27 10:40:18 -0300

    feat: add StorageProvider

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 3333
 
 app.use(cors())
 app.use(express.json())
-app.use('/files', express.static(uploadConfig.directory))
+app.use('/files', express.static(uploadConfig.uploadsFolder))
 app.use(routes)
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {

3500f96456d29bfc026769f2fc0d7899660a2a68
Alexsandro G Bezerra <alexsandro.bezerra@outlook.com.br>
2020-11-30 21:15:39 -0300

    feat: not list passed hours as available

--- src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
+++ src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
@@ -16,12 +16,16 @@ describe('ListProviderDayAvailabilityService', () => {
   it('should be able to list the mouth availability from a provider', async () => {
     await fakeAppointmentsRepository.create({
       providerId: 'user',
-      date: new Date(2020, 3, 20, 8, 0, 0)
+      date: new Date(2020, 3, 20, 14, 0, 0)
     })
 
     await fakeAppointmentsRepository.create({
       providerId: 'user',
-      date: new Date(2020, 3, 20, 10, 0, 0)
+      date: new Date(2020, 3, 20, 15, 0, 0)
+    })
+
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 3, 20, 11).getTime()
     })
 
     const availability = await listProviderDayAvailability.execute({
@@ -34,9 +38,12 @@ describe('ListProviderDayAvailabilityService', () => {
     expect(availability).toEqual(
       expect.arrayContaining([
         { hour: 8, available: false },
-        { hour: 9, available: true },
+        { hour: 9, available: false },
         { hour: 10, available: false },
-        { hour: 11, available: true }
+        { hour: 13, available: true },
+        { hour: 14, available: false },
+        { hour: 15, available: false },
+        { hour: 16, available: true }
       ])
     )
   })

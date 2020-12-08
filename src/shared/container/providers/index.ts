1155d103c7c610622b20de98a61dbbfb0aeec3a5
Alexsandro G Bezerra <alexsandro.bezerra@outlook.com.br>
2020-12-08 15:37:11 -0300

    refactor: providers containers

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,33 +1,4 @@
-import { container } from 'tsyringe'
-
-import IStorageProvider from './StorageProvider/models/IStorageProvider'
-import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider'
-
-import IMailProvider from './MailProvider/models/IMailProvider'
-import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider'
-
-import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider'
-import HandlebarsTemplateMailProvider from './MailTemplateProvider/implementations/HandlebarsTemplateMailProvider'
-
-import ICacheProvider from './CacheProvider/models/ICacheProvider'
-import RedisCacheProvider from './CacheProvider/implementations/RedisCacheProvider'
-
-container.registerSingleton<IStorageProvider>(
-  'StorageProvider',
-  DiskStorageProvider
-)
-
-container.registerSingleton<IMailTemplateProvider>(
-  'MailTemplateProvider',
-  HandlebarsTemplateMailProvider
-)
-
-container.registerInstance<IMailProvider>(
-  'MailProvider',
-  container.resolve(EtherealMailProvider)
-)
-
-container.registerInstance<ICacheProvider>(
-  'CacheProvider',
-  container.resolve(RedisCacheProvider)
-)
+import './StorageProvider'
+import './MailTemplateProvider'
+import './MailProvider'
+import './CacheProvider'

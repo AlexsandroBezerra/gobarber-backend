9d277c5a948e9ae4ef2b73d3add7a9843d584394
Alexsandro G Bezerra <alexsandro.g.bezerra@gmail.com>
2020-12-10 21:14:01 -0300

    feat: edit PasswordReset url

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -50,7 +50,7 @@ export default class SendForgotPasswordEmailService {
         file: forgotPasswordTemplate,
         variables: {
           name: user.name,
-          link: `${process.env.APP_WEB_URL}/reset?token=${token}`
+          link: `${process.env.APP_WEB_URL}/reset-password?token=${token}`
         }
       }
     })
